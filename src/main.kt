045309d17eac66d1158e6a1be9d24e60341285d1
margopon <rita.ponasenko@mail.ru>
2021-01-21 19:22:09 +0700

    Решение задач

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,41 @@
-fun main()//заглавная функция программы
-{//начало программы
- var a=100// var - variable - изменяемый
-    val b=200 //val - value - начение - константа- неизменяемый
-    print("Значение переменной = ")
-    println(a) // ывод на экран с отступом
-    print("Значение переменной b =")
-    print(b)
-
-    a=25
-    print(a)// вывод на экран без отступа на след строку
-//это первое изменение +mail
-//это второе изменение
-}//конец программы
-//да
+////fun main()//заглавная функция программы
+////{//начало программы
+//// var a=100// var - variable - изменяемый
+////    val b=200 //val - value - начение - константа- неизменяемый
+////    print("Значение переменной = ")
+////    println(a) // ывод на экран с отступом
+////    print("Значение переменной b =")
+////    print(b)
+////
+////    a=25
+////    print(a)// вывод на экран без отступа на след строку
+//////это первое изменение +mail
+//////это второе изменение
+////}//конец программы
+//////да
+//
+////{
+////    fun main()
+////    var T = readLine()!!.toDouble()
+////    var TC = (T-32)*5/9
+////    print(TC)
+////
+////}
+//        fun main() {
+//        print("speed 1")
+//        var V1 = readLine()!!.toDouble()
+//    print("speed 2")
+//        var V2 = readLine()!!.toDouble()
+//        print("distance")
+//        var S_begin = readLine()!!.toDouble()
+//        print("time")
+//        var T = readLine()!!.toDouble()
+//        var S_final = S_begin+V1*T+V2*T
+//        print(S_final)
+////}
+    import kotlin.random.Random
+    fun main() {
+    var Week = readLine()!!.toInt()
+    var Year = readLine()!!.toInt()
+    var a = Random.nextInt(3,9)
+}
